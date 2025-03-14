1bbc7d844568431179cd5af41be44435128de22b
Ruan Victor <ruan.marques@4mti.com.br>
2025-03-14 15:19:35 -0300

    refactor: remove unused DTOs and service files for users, orders, payments, products, categories, and inventory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,6 @@
-import { ExpressAdapter } from '@bull-board/express';
-import { BullBoardModule } from '@bull-board/nestjs';
-import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './modules/products/products.module';
-import { QueueModule } from './queue/queue.module';
-import { UsersModule } from './modules/users/users.module';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { PaymentModule } from './modules/payment/payment.module';
-import { OrdersModule } from './modules/orders/orders.module';
-import { InventoryModule } from './modules/inventory/inventory.module';
 
 @Module({
   imports: [
@@ -29,29 +17,8 @@ import { InventoryModule } from './modules/inventory/inventory.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
-      },
-      defaultJobOptions: {
-        attempts: 5,
-        backoff: { type: 'exponential', delay: 1000 },
-      },
-    }),
-    BullBoardModule.forRoot({
-      route: '/queue',
-      adapter: ExpressAdapter,
-    }),
-    ProductsModule,
-    QueueModule,
-    UsersModule,
-    CategoriesModule,
-    PaymentModule,
-    OrdersModule,
-    InventoryModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
